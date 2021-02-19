f11d88a2b3779198ef2a3315736afbb60fb9a3d7
thom8047 <thom8047@bears.unco.edu>
2021-02-18 23:43:14 -0700

    Updated to have game intact, no point system, no start/ higher class to oversea

--- code.js
+++ code.js
@@ -16,6 +16,8 @@ class Pong
     constructor(difficulty) 
     {
         this.difficulty = difficulty
+        this.userPoints = 0;
+        this.compPoints = 0;
 
         this.game = new Game();
     }
@@ -26,25 +28,38 @@ class Pong
         document.getElementById('display').innerHTML = string;
         return mouse_y;//height);
     }
-    getBallPos(position, current_y)
+    getBallPos(position, current_y, comp_y)
     {
         var [x, y, x_dir, y_dir, speed] = position;
         if (x >= this.game.canvas.width)
         {
-            x_dir = x_dir * -1;
+            if ((y-comp_y) > -25 && (y-comp_y) < 25)
+            {
+                var coin = Math.random(), dir;
+                
+                if (coin > 0.6) { y_dir = y_dir * 1 } else { y_dir = y_dir * -1 }
+                x_dir = x_dir * -1;
+            }
+            else 
+            {
+                return ['false', 1];
+            }
         }
         else if (x <= 0)
         {
             if ((y-current_y) > -25 && (y-current_y) < 25)
             {
+                var coin = Math.random(), dir;
+                
+                if (coin > 0.6) { y_dir = y_dir * 1 } else { y_dir = y_dir * -1 }
                 x_dir = x_dir * -1;
                 speed += (Math.random() * 0.25);
-                console.log(speed, (y-current_y));
+                //console.log(speed, (y-current_y));
             }
             else 
             {
-                console.log(y-current_y);
-                return false;
+                //console.log(y-current_y);
+                return ['false', 0];
             }
         }
         x = x + (speed * x_dir)
@@ -57,13 +72,13 @@ class Pong
         {
             y_dir = y_dir * -1;
         }
-        y = y + (y_dir)
+        y = y + ((speed*2) * y_dir)
 
 
         return [x, y, x_dir, y_dir, speed];
 
     }
-    render(context, current_y, canvas, ball_x, ball_y)
+    render(context, current_y, canvas, ball_x, ball_y, comp_y)
     {
         var x = 0, y = current_y, len = 50;
         //canvas
@@ -81,10 +96,17 @@ class Pong
         context.fillRect(canvas.width/2, 260, 3, canvas.height/10);
         context.fillStyle = 'white';
         context.fillRect(canvas.width/2, 340, 3, canvas.height/10);
+
+        context.fillStyle = 'black';
+        context.fillText("User: " + this.userPoints, canvas.width/3, canvas.height/4);
+        context.fillStyle = 'black';
+        context.fillText("Com: " + this.compPoints, canvas.width*(2/3), canvas.height/4);
         //user
         context.fillStyle = 'black';
         context.fillRect(x, y-(len/2), 8, len);
         //enemy
+        context.fillStyle = 'black';
+        context.fillRect(canvas.width-8, comp_y-(len/2), 8, len);
         //ball
         context.beginPath();
         context.arc(ball_x, ball_y, 10, 0, Math.PI*2);
@@ -96,7 +118,7 @@ class Pong
         var canvas = this.game.canvas;
         var context = this.game.context;
         var game_int;
-        var mouse_y, current_y = 0, dampening = 20, time = 10, ball_origin = [canvas.width/2, canvas.height/2, -1, 1, 1];
+        var mouse_y, current_y = 0, dampening = 20, time = 10, ball_origin = [canvas.width/2, canvas.height/2, -1, 1, 1], comp_y = 0, comp_dampening = 10;
         setTimeout(() => {
             this.game.canvas.onmousemove = function(event) 
             {
@@ -114,16 +136,27 @@ class Pong
                 var diff = (Math.abs(mouse_y-current_y)/dampening);
                 current_y -= diff;
             }
+            // getCompPos
+            if (ball_origin[1] > comp_y)
+            {
+                var diff = (Math.abs(ball_origin[1]-comp_y)/comp_dampening);
+                comp_y += diff;
+            }
+            else if (ball_origin[1] < comp_y)
+            {
+                var diff = (Math.abs(ball_origin[1]-comp_y)/comp_dampening);
+                comp_y -= diff;
+            }
         });
 
         game_int = setInterval(() => {
-            ball_origin = this.getBallPos(ball_origin, current_y)
+            ball_origin = this.getBallPos(ball_origin, current_y, comp_y)
             if (ball_origin == false)
             {
                 clearInterval(game_int);
                 console.log('end')
             }
-            this.render(context, current_y, canvas, ball_origin[0], ball_origin[1])
+            this.render(context, current_y, canvas, ball_origin[0], ball_origin[1], comp_y)
         }, time);
     }
 }
